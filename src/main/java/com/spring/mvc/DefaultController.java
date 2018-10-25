de4394a15b4d5e8a9a7551c684c7621d8d749d45
XDZL <40530294+XDZL@users.noreply.github.com>
2018-10-26 00:27:17 +0800

    Initial commit

---
+++ src/main/java/com/spring/mvc/DefaultController.java
@@ -0,0 +1,31 @@
+package com.spring.mvc;
+
+import com.dj.web.model.User;
+import com.dj.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping(value = "/one", method = RequestMethod.GET)
+public class DefaultController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/mybatis")
+    public ModelAndView indexView(@RequestParam String id, HttpServletResponse response){
+        response.setCharacterEncoding("utf-8");
+        User user = userService.getUserUsingId(id);
+        ModelAndView modelAndView = new ModelAndView("hello");
+        modelAndView.addObject("user",user);
+        return modelAndView;
+    }
+}
---
+++ web/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<h1>${user.id}</h1>
+<h2>${user.command}</h2>
+<h3>${user.content}</h3>
+<h4>${user.description}</h4>
+
+</body>
+</html>
